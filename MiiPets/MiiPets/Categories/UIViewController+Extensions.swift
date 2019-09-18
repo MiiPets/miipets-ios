1529820546a82df9c3c0b6db6573266970bd3b04
Stefan-Bouwer <sbouwer@ymail.com>
2019-09-18 21:49:07 +0200

    Fix miipets green top faded view top starting point

--- MiiPets/MiiPets/Categories/UIViewController+Extensions.swift
+++ MiiPets/MiiPets/Categories/UIViewController+Extensions.swift
@@ -2,6 +2,14 @@ import UIKit
 
 extension UIViewController {
     
+    var topAnchorConstant: CGFloat {
+        if self is UITableViewController {
+            return 50.0
+        }
+        
+        return 10.0
+    }
+    
     var miiPetsImageViewHeightConstraint: CGFloat {
         return self.view.frame.width / 2.1
     }
@@ -14,7 +22,7 @@ extension UIViewController {
         
         greenFadingImageView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: -10.0).isActive = true
         greenFadingImageView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 10.0).isActive = true
-        greenFadingImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: -10.0).isActive = true
+        greenFadingImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: -self.topAnchorConstant).isActive = true
         greenFadingImageView.heightAnchor.constraint(equalToConstant: self.miiPetsImageViewHeightConstraint).isActive = true
     }
 }
