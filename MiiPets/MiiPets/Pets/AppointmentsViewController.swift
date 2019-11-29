384e0cc4a3381b3ebb96d191e695f82afabdeaaf
Stefan-Bouwer <sbouwer@ymail.com>
2019-11-29 16:32:08 +0200

    Made AppointmentsVC a subclass of UITVC

--- MiiPets/MiiPets/Pets/AppointmentsViewController.swift
+++ MiiPets/MiiPets/Pets/AppointmentsViewController.swift
@@ -1,6 +1,12 @@
 import UIKit
 
-class AppointmentsViewController: UIViewController {
+class AppointmentsViewController: UITableViewController {
+    
+    // MARK: Constants
+    
+    let kTitleHeaderFooterViewReuseIdentifier = "TitleHeaderFooterViewReuseIdentifier"
+    
+    // MARK: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -8,6 +14,7 @@ class AppointmentsViewController: UIViewController {
         self.view.backgroundColor = UIColor.white
         
         self.addTopMiiPetsGreenFadingImage()
+        self.registerViewsAndCells()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -18,5 +25,64 @@ class AppointmentsViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    // MARK: UITableView
+    
+    private func registerViewsAndCells() {
+        self.tableView.register(LandingTitleHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: kTitleHeaderFooterViewReuseIdentifier)
+    }
+}
+
+// MARK: UITableViewDataSource
+
+extension AppointmentsViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension AppointmentsViewController {
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40.0
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: kTitleHeaderFooterViewReuseIdentifier) as? LandingTitleHeaderFooterView else {
+            return nil
+        }
+        
+        header.setTitle("MiiPointments")
+        header.updateTopPadding()
+        
+        return header
     }
 }
